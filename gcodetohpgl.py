abe1130e3854fc4a4d8b4c70bb010e24669d3ef4
Andrew G. Meyer <agmlego@gmail.com>
2013-11-22 15:45:59 -0500

    Added generation for routing and milling layers, restructured generation and control functions.

--- gcodetohpgl.py
+++ gcodetohpgl.py
@@ -2,14 +2,14 @@
 
 import sys
 import re
-import serial
 import io
 from tempfile import SpooledTemporaryFile as sptf
 from argparse import ArgumentParser
 from glob import glob
 from os.path import join
-from termcolor import colored, cprint
 from time import sleep
+from termcolor import colored, cprint
+import serial
 
 #### Machine configuration ####
 # default units for moves are inches
@@ -35,8 +35,8 @@ mill_feed = 12000
 # spindle speed in krpm [0..32]
 spindle_speed = 32
 
-# time to wait, in ms, for a drill to complete
-drill_dwell = 700
+# time to wait, in seconds, for a drill to complete
+drill_dwell = 0.7
 
 # number of HPGL commands to buffer before waiting
 serial_queue = 1
@@ -88,22 +88,26 @@ def parse_move(gcode):
         newx = int(calfactor * (xoff + float(xycmd.group(1))))
         newy = int(calfactor * (yoff + float(xycmd.group(2))))
         if newx > xmax:
-            sys.stderr.write('X move bigger than bed! (%d > %d)\n'%(newx,xmax))
+            cprint('X move bigger than bed! (%d > %d)\n'%(newx,xmax),
+                   'red',file=sys.stderr)
             sys.exit(12)
         if newy > ymax:
-            sys.stderr.write('Y move bigger than bed! (%d > %d)\n'%(newy,ymax))
+            cprint('Y move bigger than bed! (%d > %d)\n'%(newy,ymax),
+                   'red',file=sys.stderr)
             sys.exit(12)
         hpgl = 'PA%d,%d;'%(newx,newy)
     elif mode == 'rel':
         newx = int(calfactor * (float(xycmd.group(1))))
         newy = int(calfactor * (float(xycmd.group(2))))
         if parse_move.x + newx > xmax:
-            sys.stderr.write('X move bigger than bed! (%d > %d)\n'\
-                             %(newx,xmax))
+            cprint('X move bigger than bed! (%d > %d)\n'\
+                   %(parse_move.x + newx,xmax),
+                   'red',file=sys.stderr)
             sys.exit(12)
         if parse_move.y + newy > ymax:
-            sys.stderr.write('Y move bigger than bed! (%d > %d)\n'\
-                             %(parse_move.y + newy,ymax))
+            cprint('Y move bigger than bed! (%d > %d)\n'\
+                   %(parse_move.y + newy,ymax),
+                   'red',file=sys.stderr)
             sys.exit(12)
         hpgl = 'PR%d,%d;'%(parse_move.x + newx,parse_move.y + newy)
     parse_move.x += newx
@@ -118,19 +122,30 @@ def parse_z(gcode,drill):
     if newz <= 0.0: 
        hpgl = 'PD;'
        if drill:
-           hpgl += '!TW%s;'%drill_dwell
+           hpgl += parse_dwell('G04 P%s'%drill_dwell)
     if newz > 0.0:
        hpgl = 'PU;'
     return hpgl
 
+def parse_dwell(line):
+    '''Parse a dwell command'''
+    t = float(line[line.find('P')+1:])*1000
+    return '!TW%.0f;'%t
+
 def parse_tool_change(gcode):
     '''Parse a tool-change command'''
-    global spindle_speed
     newtool = re.match("M06 T([0-9]*) \((.*)\).*", gcode)
-    hpgl = '!OC;!RM0;!CC;!EM0;PA0,0;'+\
-           '\nCO "insert tool %s: size '%newtool.group(1).strip() + newtool.group(2).strip() + '"' +\
-           '\n!OC;!RM%d;!CC;!EM1;'%spindle_speed
+    hpgl = '\nCO "Insert tool #%s: size %s"\n'\
+                %(newtool.group(1).strip(),newtool.group(2).strip())
     return hpgl
+
+def parse_spindle(start):
+    '''Start or stop the spindle'''
+    global spindle_speed
+    if start:
+        return '!OC;!RM%d;!CC;!EM1;'%spindle_speed
+    else:
+        return '!OC;!RM0;!CC;!EM0;PA0,0;'
 
 def parse_line(line,drill):
     '''Parse a line of GCODE'''
@@ -157,13 +172,13 @@ def parse_line(line,drill):
             hpgl = parse_move(line)
     elif line.startswith('G04'):
         # dwell
-        pass
+        hpgl = parse_dwell(line)
     elif line.startswith('M03'):
         # start spindle
-        pass
+        hpgl = parse_spindle(start=True)
     elif line.startswith('M05'):
         # stop spindle
-        pass
+        hpgl = parse_spindle(start=False)
     elif line.startswith('M06'):
         # tool change
         hpgl = parse_tool_change(line)
@@ -174,8 +189,16 @@ def parse_line(line,drill):
 #### Start control functions ####
 def tool_change(hpgl):
     '''Change tools'''
-    drill = hpgl[hpgl.find('"')+1:hpgl.rfind(' ')]
-    raw_input('Insert %s" diameter drill\nPress enter when done.')
+    drill = hpgl[hpgl.find('"')+1:hpgl.rfind('"')]
+    raw_input('%s\nPress enter when done.'%drill)
+
+def send_cmd(hpgl,wait=0.01):
+    '''Send a command and wait for the response from the machine'''
+    ser.write(hpgl + ";")
+    if hpgl not in ('!OC','!WR0,8,8'):
+        while ser.inWaiting() == 0:
+            sleep(wait)
+    print '%s\t%s'%(hpgl,ser.read(ser.inWaiting()))
 
 #### End control functions ####
 
@@ -183,14 +206,19 @@ def main():
     '''Main program routine'''
     #### Set up command-line arguments and usage instructions ####
     parser = ArgumentParser(description='Converts EAGLE GCODE (from '
-                            'http://pcbgcode.com) into LPKF HGPL, and runs the '
-                            'machine', usage='%(prog)s [options] DIR')
+                            'http://pcbgcode.com) into LPKF HGPL, and runs the'
+                            ' machine', usage='%(prog)s [options] DIR')
     parser.add_argument('gcode_dir',metavar='DIR',default='./',
                         help='directory in which the gcode files reside')
-    parser.add_argument('-o','--output',metavar='FILE',dest='save_hpgl',default='$$$$TEMP$$$$',
-                        help='save the HPGL output in the given location (defaults to a temp file)')
+    parser.add_argument('-d','--dry-run',dest='dry',action='store_true',
+                        default=False,help='only generates HPGL, no machine'
+                        ' control')
+    parser.add_argument('-o','--output',metavar='FILE',dest='save_hpgl',
+                        default='$$$$TEMP$$$$',help='save the HPGL output in'
+                        ' the given location (defaults to a temp file)')
     parser.add_argument('-f','--file',dest='file', default='',
-                        help='which file prefix to use out of the gcode files in the directory')
+                        help='which file prefix to use out of the gcode'
+                        ' files in the directory')
     ser_opts = parser.add_argument_group('serial port options')
     ser_opts.add_argument('-p','--port',dest='port',default='/dev/ttyUSB0',
                         help='serial port (default /dev/ttyUSB0)')
@@ -206,8 +234,7 @@ def main():
     print '%s Start GCODE Processing %s'%('-'*28,'-'*28)
     drills = glob(join(args.gcode_dir,args.file + '*drill.g'))
     routes = glob(join(args.gcode_dir,args.file + '*etch.g'))
-    mills = glob(join(args.gcode_dir,args.file + '*.g'))
-    mills = list(set(mills) - set(drills) - set(routes))
+    mills = glob(join(args.gcode_dir,args.file + '*mill.g'))
     
     if len(drills) > 1:
         sys.stderr.write('Multiple drill files selected, too confusing!\n\t')
@@ -215,6 +242,7 @@ def main():
         sys.exit(10)
     
     layer = drills[0][-11:-8]
+    olayer = ('top','bot')[layer == 'top']
     
     print 'Using layer %s as first layer based on drill file.'%layer 
     print 'Machine is on %s at %dbaud'%(args.port,args.baud)
@@ -233,13 +261,12 @@ def main():
         hpgl_file = open(args.save_hpgl,'w+b')
         print 'Producing HPGL output in %s'%args.save_hpgl
 
+    hpgl_file.write('IN;!CT1;VS%d;!OC;!SV140;!SM32;!WR0,8,8;!CC;!CM1;'
+                    '!EM1;!OC;!RM%d;!CC;'%(mill_feed,spindle_speed))
+    number = 0
     # drills first
     with open(drills[0]) as f:
         print '%s Drills %s'%('='*36,'='*36)
-        number = 0
-    
-        hpgl_file.write('IN;!CT1;VS%d;!OC;!SV140;!SM32;!WR0,8,8;!CC;!CM1;!EM1;!OC;!RM%d;!CC;'%(mill_feed,spindle_speed))
-    
         for line in f:
             line = line.strip()
             if line.startswith('('):
@@ -251,45 +278,75 @@ def main():
             hpgl_file.write(hpgl)
         print '%s End Drills %s'%('='*34,'='*34)
 
+    parse_tool_change('M06 T98 (routing )')
+
+    # routing on the drill layer next
+    with open([f for f in routes if layer in f][0]) as f:
+        print '%s %s Traces %s'%('='*34,layer,'='*34)
+        for line in f:
+            line = line.strip()
+            if line.startswith('('):
+                #comment
+                continue
+            hpgl = parse_line(line,drill=False)
+            number += 1
+            print '%d\t%s\t%s%s'%(number,line,('','\t')[len(line)<16],hpgl)
+            hpgl_file.write(hpgl)
+        print '%s End %s Traces %s'%('='*32,layer,'='*32)
+
+    # routing on other layer
+    with open([f for f in routes if olayer in f][0]) as f:
+        print '%s %s Traces %s'%('='*34,olayer,'='*34)
+        for line in f:
+            line = line.strip()
+            if line.startswith('('):
+                #comment
+                continue
+            hpgl = parse_line(line,drill=False)
+            number += 1
+            print '%d\t%s\t\t%s%s'%(number,line,('','\t')[len(line)<16],hpgl)
+            hpgl_file.write(hpgl)
+        print '%s End %s Traces %s'%('='*32,olayer,'='*32)
+    
+    parse_tool_change('M06 T99 (milling )')
+    
+    # milling layer last
+    with open([f for f in mills if olayer in f][0]) as f:
+        print '%s %s Traces %s'%('='*34,layer,'='*34)
+        for line in f:
+            line = line.strip()
+            if line.startswith('('):
+                #comment
+                continue
+            hpgl = parse_line(line,drill=False)
+            number += 1
+            print '%d\t%s\t%s%s'%(number,line,('','\t')[len(line)<16],hpgl)
+            hpgl_file.write(hpgl)
+        print '%s End %s Traces %s'%('='*32,layer,'='*32)
+
     hpgl_file.write('!OC;!RM0;!CC;PU;!EM0;PA0,0;')
 
     #### End HPGL generation ####
 
     #### Serial output (machine control) ####
-    if args.save_hpgl == '$$$$TEMP$$$$':
+    if not args.dry:
         ser = serial.Serial(args.port,args.baud,rtscts=True)
+        
+        # discard anything in the input buffer before starting
         ser.read(ser.inWaiting())
-        queued = 0
         hpgl_file.seek(0, 0)
         for line in hpgl_file.read().split('\n'):
             print repr(line.strip())
             for command in line.split(";"):
             	if command:
-                    comment = re.match("CO \"(.*)\"", command)
-                    if comment:
-                        raw_input(colored(comment.group(1), 'magenta'))
-                    elif 'RM' in command:
-                        ser.write(command  + ';')
-                        while ser.inWaiting() == 0:
-                            sleep(0.01)
-                        code = ser.read(ser.inWaiting())
-                        match = True
-                        while match:
-                            print code
-                            ser.write(command  + ';')
-                            while ser.inWaiting() == 0:
-                                sleep(0.01)
-                            newcode = ser.read(ser.inWaiting())
-                            match = (code == newcode)
+                    if command.startswith('CO'):
+                        if 'toolchange' in command:
+                            tool_change(command)
                     else:
-                        ser.write(command + ";")
-                        queued += 1
-                        if command not in ('!OC','!WR0,8,8'):
-                            while ser.inWaiting() == 0:
-                                sleep(1)
-                        print colored(command, 'red'),colored('%r'%ser.read(ser.inWaiting()),'yellow')
-        print('Wait until plotter finishes and press enter to exit')
-        raw_input()
+                        send_cmd(command)
+        raw_input('Wait until plotter finishes and press enter to exit')
+    else:
+        print colored('Machine control disabled, dry run!', 'yellow')
     #### End Machine control ####
 
 if __name__ == '__main__':
